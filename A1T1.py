cc94dc769185fb08e48c88353ecbc4bd650d6f4d
Dip6023 <dip109678@gmail.com>
2025-04-11 20:09:18 +0530

    Add files via upload

---
+++ A1T1.py
@@ -0,0 +1,13 @@
+#Task 1
+x=input('Number 1: ')
+y=input('Number 2: ')
+x=int(x)
+y=int(y)
+A= x+y
+print('Addition: ',A)
+S=x-y
+print('Subtraction: ',S)
+M=x*y
+print('Multiplication: ',M)
+D=x/y
+print('Division: ',D)
---
+++ assaignment  1.py
@@ -0,0 +1,4 @@
+#Task 2
+name=input('Enter Your first name')
+surname=input(' Enter Your Last name')
+print("Hello,"+name+ surname+"!"+"Welcome to the python progam.")
