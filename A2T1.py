e05dcea73f375cee2828758911b0c0b46b1e6a6d
Dip6023 <dip109678@gmail.com>
2025-04-12 21:32:17 +0530

    Add files via upload

---
+++ A2T1.py
@@ -0,0 +1,5 @@
+a=int(input('Enter a number: '))
+if a%2==1:
+    print('This is a odd number')
+else:
+    print('This is a even number')
---
+++ A2T2.py
@@ -0,0 +1,5 @@
+#Task 2
+result=0
+for x in range(1,51):
+    result = result + x
+    print('The Sum of numbers from 1 to 50 is: ',result)
